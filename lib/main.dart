915b0ebfa66bde86bf7dc24ad3d698a84ce5002e
model9c <model9c@gmail.com>
2023-05-19 16:48:09 +0900

    -init -how to use logger

--- lib/main.dart
+++ lib/main.dart
@@ -1,26 +1,45 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/widgets.dart';
+import 'package:tomato_record/splash_screen.dart';
+import 'package:tomato_record/utils/logger.dart';
 
 void main(){
+  logger.d('My first Logger!!');
   runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget{
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      home: Scaffold(
-        backgroundColor: Colors.red,
-      ),
+    return FutureBuilder(
+      future: Future.delayed(const Duration(seconds: 3), () => 100),
+      builder: (context, snapshot) {
+        return AnimatedSwitcher(
+          duration: const Duration(milliseconds: 300),
+          child: _splashLoadingWidget(snapshot)
+        );
+      }
     );
+  }
+
+  StatelessWidget _splashLoadingWidget(AsyncSnapshot<Object?> snapshot) {
+    if(snapshot.hasError){
+      print('error occur while loading.');
+      return const Text('error occur.');
+    } else if(snapshot.hasData) {
+      return const TamoiApp();
+    } else {
+      return const SplashScreen();
+    }
   }
 }
 
-class SplashScreen extends StatelessWidget {
-  const SplashScreen({Key? key}) : super(key: key);
+class TamoiApp extends StatelessWidget {
+  const TamoiApp({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Container(
+      color: Colors.amber,
+    );
   }
 }
